737adc3126862fc034ec5d629a74c610fa7d6e63
synduil <202508850729@alunos.estacio.br>
2025-08-26 23:22:15 +0000

    fiz os primeiros passos, detalhes da 1 carta

--- super_trunfos.c
+++ super_trunfos.c
@@ -3,6 +3,10 @@
 int main(){
     //carta:1//
     char Estado;
-    
-    
+    char Codigo[10];
+    char Cidade[20];
+    int Populacao;
+    float Area;
+    float PIB;
+    int PontosTuristicos;
 }
